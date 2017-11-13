25d1f3e0643d092dcc3ac653b0c54a5693c25c93
juliogv14 <id00681011@usal.es>
2017-11-14 00:07:46 +0100

    Added login xml

---
+++ app/src/main/java/com/juliogv14/turnosync/LoginActivity.java
@@ -0,0 +1,11 @@
+package com.juliogv14.turnosync;
+
+import android.app.Activity;
+
+/**
+ * Created by Julio on 14/11/2017.
+ * MainActivity.java
+ */
+
+public class LoginActivity extends Activity {
+}
