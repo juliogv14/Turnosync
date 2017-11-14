d2b130514e4f8d64675f402744f1210499ca8647
juliogv14 <id00681011@usal.es>
2017-11-14 21:23:21 +0100

    Added login clicklisteners on button and edittext done

--- app/src/main/java/com/juliogv14/turnosync/LoginActivity.java
+++ app/src/main/java/com/juliogv14/turnosync/LoginActivity.java
@@ -1,11 +1,67 @@
 package com.juliogv14.turnosync;
 
-import android.app.Activity;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.os.AsyncTask;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.TextView;
 
 /**
  * Created by Julio on 14/11/2017.
- * MainActivity.java
+ * LoginActivity.java
  */
 
-public class LoginActivity extends Activity {
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mTextViewEmail;
+    private EditText mTextViewPassword;
+    private FrameLayout mProgressOverlay;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //FindView
+
+        mTextViewPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        findViewById(R.id.button_login).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptLogin();
+            }
+        });
+
+
+    }
+
+    private void attemptLogin() {
+
+    }
+
+
+
+
+
+
 }
