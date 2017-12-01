d91c71af887b3805f20812418f877e8782d40e7c
juliogv14 <id00681011@usal.es>
2017-12-01 19:19:07 +0100

    Add settings empty fragment and dependency

---
+++ app/src/main/java/com/juliogv14/turnosync/Settings/SettingsFragment.java
@@ -0,0 +1,23 @@
+package com.juliogv14.turnosync.Settings;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.preference.PreferenceFragmentCompat;
+
+/**
+ * Created by Julio on 01/12/2017.
+ * SettingsFragment.class
+ */
+
+public class SettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+    }
+}
